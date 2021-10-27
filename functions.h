ef176ec0142f4e3243eef1362330c571382fab76
pershinpv <pershinpv@gmail.com>
2021-10-27 19:49:57 +0000

    Include guard. Fixes #1

--- functions.h
+++ functions.h
@@ -1,6 +1,12 @@
+
+#ifndef MYHEADER
+#define MYHEADER
+
 struct vector3d { float x, y, z; };
 
 struct vector3d sum3d(struct vector3d* vect_a, struct vector3d* vect_b);
 struct vector3d sub3d(struct vector3d* vect_a, struct vector3d* vect_b);
 float dot3d(struct vector3d* vect_a, struct vector3d* vect_b);
 struct vector3d cross3d(struct vector3d* vect_a, struct vector3d* vect_b);
+
+#endif
